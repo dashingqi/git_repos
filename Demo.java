24a3f068fc4530046014b0534773dd68a6d6c4cf
zhangqi61 <zhangqi61@baidu.com>
2022-05-26 16:01:22 +0800

    [update]

--- Demo.java
+++ Demo.java
@@ -4,10 +4,13 @@ class People{
 
 		// ===========第一次提交===========
 		System.out.println("first");
+<<<<<<< HEAD
 
 		// ===========第二次提交===========
 		System.out.println("second");
 		// ===========第三次提交===========
 		System.out.println("thired");
+=======
+>>>>>>> parent of fcd2c87 ([update second])
 	}
 }
